e358fe91ead3baa024ba3a6e42f473729cdc57ef
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-10-18 10:13:26 -0500

    fmt changes

--- client/v1/gatypes/timing.go
+++ client/v1/gatypes/timing.go
@@ -6,12 +6,12 @@ type Timing struct { // t=timing
 	UserTimingVariableName string `url:"utv,omitempty"`
 	UserTimingTime         int64  `url:"utt,omitempty"`
 	UserTimingLabel        string `url:"utl,omitempty"`
-	PageLoadTime         int64 `url:"plt,omitempty"`
-	DNSTime              int64 `url:"dns,omitempty"`
-	PageDownloadTime     int64 `url:"pdt,omitempty"`
-	RedirectResponseTime int64 `url:"rrt,omitempty"`
-	TCPConnectTime       int64 `url:"tcp,omitempty"`
-	ServerResponseTime   int64 `url:"srt,omitempty"`
-	DOMInteractiveTime   int64 `url:"dit,omitempty"`
-	ContentLoadTime      int64 `url:"clt,omitempty"`
+	PageLoadTime           int64  `url:"plt,omitempty"`
+	DNSTime                int64  `url:"dns,omitempty"`
+	PageDownloadTime       int64  `url:"pdt,omitempty"`
+	RedirectResponseTime   int64  `url:"rrt,omitempty"`
+	TCPConnectTime         int64  `url:"tcp,omitempty"`
+	ServerResponseTime     int64  `url:"srt,omitempty"`
+	DOMInteractiveTime     int64  `url:"dit,omitempty"`
+	ContentLoadTime        int64  `url:"clt,omitempty"`
 }
